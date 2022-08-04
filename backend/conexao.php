2fb850ea5841d6f51a585242fcc910a2682354f7
izabelcsp <101677367+izabelcsp@users.noreply.github.com>
2022-08-03 21:11:32 -0300

    correção echo

--- backend/conexao.php
+++ backend/conexao.php
@@ -10,7 +10,7 @@ try{
     $con = new PDO("mysql:host=".SERVIDOR.";dbname=".BASEDEDADOS, USUARIO, SENHA);
     // defina o modo de erro PDO para exceção
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conectado com sucesso!";
+    // echo "Conectado com sucesso!";
 
 }catch(PDOException $erro){
     echo "Erro ao conectar no banco de dados".$erro->getMessage();
