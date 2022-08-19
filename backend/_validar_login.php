ace9a2c70d8a84e544c5e9b1ab09b379a07c3716
izabelcsp <101677367+izabelcsp@users.noreply.github.com>
2022-08-18 22:26:30 -0300

    validação

---
+++ backend/_validar_login.php
@@ -0,0 +1,33 @@
+<?php
+
+include 'conexao.php';
+
+try{
+    $usuario = $_POST['usuario'];
+    $senha = $_POST['senha'];
+
+    $sql = "SELECT * FROM tb_login WHERE usuario='$usuario' AND BINARY senha='$senha' AND ativo = 1 ";
+    // binary, diferencia caracteres
+
+    $comando = $conn->prepare($sql);
+    $comando->execute();
+    $dados = $comando->fetchAll(PDO::FETCH_ASSOC);
+
+    // var_dump($dados);
+
+    // verifica se existem registros dentro da variavel dados
+
+    // if($dados == null) verificar se dados é nulo
+    if($dados != null){ 
+    // verificar se dados é diferente de nulo
+    header('location:../admin/gerenciar_viagens.php');
+}else{
+    // se o usuario ou senha sao invalidos, irá entrar nesse bloco de código
+    echo "Usuário ou senha invalidos";
+    // header('location: ../admin/index.html');
+}
+
+}catch(PDOException $erro){
+    echo $erro->getMessage();
+
+}
